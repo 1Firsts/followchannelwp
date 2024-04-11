314d8290fc849adc12b9aadb794b7989ee194080
Achmad Imannudin <imannudinachmad1@gmail.com>
2024-04-12 04:07:38 +0700

    Fix Error
    Request failed: 401 Client Error

--- auto_follow.py
+++ auto_follow.py
@@ -15,27 +15,15 @@ ua = UserAgent()
 fake_user_agent = ua.random
 
 headers = {
-    'authority': 'api.warpcast.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9,id;q=0.8',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'cross-site',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': fake_user_agent,  # Use fake user agent
-    'Authorization': f'Bearer {authorization_token}'  # Gunakan token Authorization yang dimasukkan pengguna
+    'User-Agent': fake_user_agent,
+    'Authorization': f'Bearer {authorization_token}',
+    'Content-Type': 'application/json'
 }
 
 # Daftar untuk menyimpan ID channel yang sudah difollow
 followed_channels = []
 
-response = requests.get(
-    'https://api.warpcast.com/v2/all-channels', headers=headers)
+response = requests.get('https://api.warpcast.com/v2/all-channels', headers=headers)
 data = json.loads(response.text)
 
 for channel in data['result']['channels']:
@@ -63,15 +51,19 @@ for channel in data['result']['channels']:
         response = requests.put(
             'https://client.warpcast.com/v2/feed-follows', headers=headers, json={"feedKey": channel['id']})
         response.raise_for_status()  # Raise an exception for bad responses
+        
         print(Fore.GREEN + f"Successfully followed channel with name {channel['id']}:")
         print(json.dumps(output, indent=4))
         print("------------------------")
         
         # Tambahkan ID channel ke dalam daftar
         followed_channels.append(channel['id'])
-    except requests.exceptions.RequestException as e:
-        print(Fore.RED + f"Failed to follow channel with name {channel['id']}. Request failed:", e)
+    except requests.exceptions.HTTPError as e:
+        print(Fore.RED + f"Failed to follow channel with name {channel['id']}. HTTP error occurred:", e.response.status_code)
+        print(e.response.text)
     except json.decoder.JSONDecodeError as e:
         print(Fore.RED + f"Failed to follow channel with name {channel['id']}. JSON decoding error:", e)
+    except Exception as e:
+        print(Fore.RED + f"Failed to follow channel with name {channel['id']}. An unexpected error occurred:", e)
     
     time.sleep(5)  # Jeda selama 5 detik
